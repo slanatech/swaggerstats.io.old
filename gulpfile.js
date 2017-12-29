060fb372fa6fafd2a7dee498272cfef916538796
sv2 <sv2@slana.tech>
2017-12-28 22:50:09 -0800

    landing

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
 var gulp = require('gulp'),
     gp_exec = require('gulp-exec'),
-    cp = require('child_process');
+    cp = require('child_process'),
+    gutil = require('gutil');
 
 // TODO Build docs
 
 // Rebuild Jekyll
 gulp.task('build-jekyll', function(code) {
     return cp.spawn('jekyll', ['build'], { stdio: 'inherit' }) // Adding incremental reduces build time.
-        .on('error', function(error) {gutil.log(gutil.colors.red(error.message))})
+        .on('error', function(error) {console.log(error.message)})
         .on('close', code);
 });
 
